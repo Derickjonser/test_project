6b2bb4ce90887c5422719ce6581a6ecc30fac981
Derick <heroesa23@gmail.com>
2022-07-27 21:17:02 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = prompt("Ти пацюк?", "");
+// console.log(typeof(answer));
+
+const answers = [];
+
+// answers[0] = prompt('Ти шо тут забув?', '');
+// answers[1] = prompt('Ти нашо тут?', '');
+// answers[2] = prompt('Підеш звідси?', '');
+
+console.log(typeof(answers));
+console.log(typeof(null));

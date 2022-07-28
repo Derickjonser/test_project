31ddf5430879141bd8e55e8c47a1d3ef0790e9fa
Derick <heroesa23@gmail.com>
2022-07-28 21:09:24 +0300

    first program

--- js/script.js
+++ js/script.js
@@ -6,11 +6,31 @@
 // const answer = prompt("Ти пацюк?", "");
 // console.log(typeof(answer));
 
-const answers = [];
+// const answers = [];
 
 // answers[0] = prompt('Ти шо тут забув?', '');
 // answers[1] = prompt('Ти нашо тут?', '');
 // answers[2] = prompt('Підеш звідси?', '');
 
-console.log(typeof(answers));
-console.log(typeof(null));
+// console.log(typeof(answers));
+// console.log(typeof(null));
+
+const numberOfFilms = prompt('Скільки фільмів ти вже подивився?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один з останніх побачених фільмів?', ''),
+      b = prompt('На скільки його оціниш?', ''),
+      c = prompt('Один з останніх побачених фільмів?', ''),
+      d = prompt('На скільки його оціниш?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
